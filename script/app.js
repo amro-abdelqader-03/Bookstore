ee1a8ebdbe566492ee48897dba7504c6ca9b4a37
amro-abdelqader-03 <amro.abdelqader.03@gmail.com>
2026-09-21 20:42:32 +0300

    add ex1

---
+++ script/app.js
@@ -0,0 +1,18 @@
+let name = prompt("Enter your name : ");
+let type = prompt("Enter your membership type (student/regular) : ");
+let title;
+
+if (type === "student")
+    title = "Scholar";
+else if (type === "regular")
+    title = "Member";
+
+if (title)
+    alert("Welcome "+name+" , and your title is "+title)
+else
+    alert("Welcome "+name+" , you don't have a title")
+
+let fiction = prompt("Are you prefers a fiction or non-fiction book genre : ")
+let book = prompt("write the specific title of the book you wants to borrow : ")
+alert("The "+borrow+" book is being reserved")
+console.log(name + " order a "+book+ "book")
