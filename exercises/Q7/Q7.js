afffde4dc70577b863eb61319fc2c73dbf26b3f0
amro-abdelqader-03 <amro.abdelqader.03@gmail.com>
2026-09-21 22:45:07 +0300

    use alert at Q7

--- exercises/Q7/Q7.js
+++ exercises/Q7/Q7.js
@@ -3,11 +3,11 @@ let year = Number(prompt("Enter your birth year"))
 let age = 2026 - year
 
 if (age >= 60)
-    console.log("You may join the seniors' program.")
+    alert("You may join the seniors' program.")
 else if (age >= 30)
-    console.log("You are not eligible. You may join other programs.");
+    alert("You are not eligible. You may join other programs.");
 else if (age >= 18 && age < 30)
-    console.log("You are eligible. Start your application.");
+    alert("You are eligible. Start your application.");
 else if (age < 18)
-    console.log("You may join the kids' program.")
+    alert("You may join the kids' program.")
     
