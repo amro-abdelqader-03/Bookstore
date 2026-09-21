c043dab358d72d9d9776c100d586301535f6c80f
amro-abdelqader-03 <amro.abdelqader.03@gmail.com>
2026-09-21 23:56:35 +0300

    update ex

--- script/app.js
+++ script/app.js
@@ -1,18 +1,21 @@
-let name = prompt("Enter your name : ");
-let type = prompt("Enter your membership type (student/regular) : ");
-let title;
+function startBorrowing() {
 
-if (type === "student")
-    title = "Scholar";
-else if (type === "regular")
-    title = "Member";
+    let name = prompt("Enter your name : ");
+    let type = prompt("Enter your membership type (student/regular) : ");
+    let title;
 
-if (title)
-    alert("Welcome "+name+" , and your title is "+title)
-else
-    alert("Welcome "+name+" , you don't have a title")
+    if (type === "student")
+        title = "Scholar";
+    else if (type === "regular")
+        title = "Member";
 
-let fiction = prompt("Are you prefers a fiction or non-fiction book genre : ")
-let book = prompt("write the specific title of the book you wants to borrow : ")
-alert("The "+borrow+" book is being reserved")
-console.log(name + " order a "+book+ "book")
+    if (title)
+        alert("Welcome " + name + " , and your title is " + title)
+    else
+        alert("Welcome " + name + " , you don't have a title")
+
+    let fiction = prompt("Are you prefers a fiction or non-fiction book genre : ")
+    let book = prompt("write the specific title of the book you wants to borrow : ")
+    alert("The " + borrow + " book is being reserved")
+    console.log("user : " + name + " order : " + book + " book / fiction : " + fiction)
+}
